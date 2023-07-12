28a0d7354f16dc088c99e1e2c9f7b90f64b5f901
Dmitry Ostapov <OstapovD@terralink-global.com>
2023-07-12 10:41:11 +0300

    1

---
+++ src/test/java/com/example/test1207/Test1207ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test1207;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test1207ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
